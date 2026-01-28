d0c049d65487f04d3087ed4455e01b8be636ce8f
RILMANIA <emir.haikal.eh@gmail.com>
2026-01-28 15:47:12 +0700

    modify centralized baseUrl and services

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChatProvider } from "./context/ChatContext";
+import LandingPage from "./pages/LandingPage";
+import ChatPage from "./pages/ChatPage";
 
+/**
+ * Main App Component
+ * Sets up routing and global context providers
+ */
 function App() {
   return (
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <ChatProvider>
+      <Router>
+        <Routes>
+          {/* Landing Page - Entry point for users */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* Chat Page - Main chat interface */}
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </Router>
+    </ChatProvider>
   );
 }
 
